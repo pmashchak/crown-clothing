d818d76684cf615a46f003df041beedfe26faed4
Pavlo Mashchak <pmashchak@gmail.com>
2020-11-29 04:05:37 -0500

    updating app with Shop page, collection preview/item components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
-import { Switch, Route, Link} from 'react-router-dom'
+import ShopPage from './pages/shop/shop.component'
 
-const HatsPage = (props) => {
-  console.log(props)
-  return(
-  <div>
-    <h1>HATS PAGE {props.match.params.id} </h1>
-    <p> <Link to='/hats/12'>Hats</Link></p>
-    <button onClick={() => props.history.push('/hats/17')}>Click</button>
-  </div>)
-}
+import { Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route path='/hats/:id' component={HatsPage}/>
+        <Route path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
