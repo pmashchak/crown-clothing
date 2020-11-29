32e555b585d6a429b7046ff6455ef5f28f936368
Pavlo Mashchak <pmashchak@gmail.com>
2020-11-28 23:47:10 -0500

    update menu item with router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+import { Switch, Route, Link} from 'react-router-dom'
+
+const HatsPage = (props) => {
+  console.log(props)
+  return(
+  <div>
+    <h1>HATS PAGE {props.match.params.id} </h1>
+    <p> <Link to='/hats/12'>Hats</Link></p>
+    <button onClick={() => props.history.push('/hats/17')}>Click</button>
+  </div>)
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/hats/:id' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
