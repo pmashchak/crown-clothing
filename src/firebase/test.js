bed0283fa408208c4dcad62b48d1e1e98e1e4af6
Pavlo Mashchak <pmashchak@gmail.com>
2020-11-30 23:44:31 -0500

    finishing sing up methods

---
+++ src/firebase/test.js
@@ -0,0 +1,7 @@
+//import firebase from 'firebase/app';
+//import 'firebase/firestore';
+
+// const firestore = firebase.firestore();
+// firestore.collection('users').doc('Y7DLYLhlrlkKpCYKA45j').collection('cartItems').doc('MRMqwIwsXZMCsZuVaBKx');
+// firestore.doc('/users/Y7DLYLhlrlkKpCYKA45j/cartItems/MRMqwIwsXZMCsZuVaBKx')
+// firestore.collection('/users/Y7DLYLhlrlkKpCYKA45j/cartItems')
